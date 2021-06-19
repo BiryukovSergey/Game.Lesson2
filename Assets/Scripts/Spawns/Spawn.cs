8b978dcc492e09fc8cf9942388e88b21caa4055c
BiryukovSergey <twc2012@yandex.ru>
2021-06-19 17:40:37 +0300

    Add GoodBonusSpawn

--- Assets/Scripts/Spawns/Spawn.cs
+++ Assets/Scripts/Spawns/Spawn.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawn : MonoBehaviour
 {
-    [SerializeField] protected Transform[] _transformsSpawnsSphere;
-    [SerializeField] internal GameObject _gameObject;
+    [SerializeField] protected Transform[] _transformsSpawnsBadSphere;
+    [SerializeField] protected Transform[] _transformsSpawnsGoodSphere;
+    [SerializeField] internal GameObject _gameObjectBadDonus;
+    [SerializeField] internal GameObject _gameObjectGoodDonus;
+    
+
+    
 
     private void Awake()
     {
-        for (int i = 0; i < _transformsSpawnsSphere.Length; ++i)
+        for (int i = 0; i < _transformsSpawnsBadSphere.Length; ++i)
         {
-            Instantiate(_gameObject, transform.position, transform.rotation);
-            transform.position = _transformsSpawnsSphere[i].transform.position;
+            Instantiate(_gameObjectBadDonus, transform.position, transform.rotation);
+            transform.position = _transformsSpawnsBadSphere[i].transform.position;
+        }
+        
+        for (int i = 0; i < _transformsSpawnsGoodSphere.Length; ++i)
+        {
+            Instantiate(_gameObjectGoodDonus, transform.position, transform.rotation);
+            transform.position = _transformsSpawnsGoodSphere[i].transform.position;
         }
     }
 }
