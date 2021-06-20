0da58ab047accc2b729c024c03205404e062cde7
BiryukovSergey <twc2012@yandex.ru>
2021-06-20 12:56:08 +0300

    Добавил парящие плиты

---
+++ Assets/Scripts/Panels/FlyPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FlyPanel : MonoBehaviour
+{
+    public Transform pos;
+    public float numeric;
+    public float numeric1;
+    private void Start()
+    {
+        numeric = Random.Range(0.5f, 2.7f);
+        numeric1 = Random.Range(0.5f, 2.7f);
+    }
+
+    private void LateUpdate()
+    {
+        pos.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time, numeric),
+            transform.localPosition.z);
+    }
+}
