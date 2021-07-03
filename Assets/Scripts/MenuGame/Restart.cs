18d62a653b14b640af6efa8bea314993ef03eb26
BiryukovSergey <twc2012@yandex.ru>
2021-07-03 19:06:45 +0300

    добавил ворота передалал UI

---
+++ Assets/Scripts/MenuGame/Restart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public Button Button;
+
+    private void Start()
+    {
+        Button.onClick.AddListener(Reset);
+    }
+
+    public void Reset()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+
+    ~Restart()
+    {
+        Button.onClick.RemoveListener(Reset);
+    }
+}
