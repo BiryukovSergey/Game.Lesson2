a594b6b95314721c94e79f5263641c2102c6e48d
BiryukovSergey <twc2012@yandex.ru>
2021-07-03 19:19:14 +0300

    Win UI

---
+++ Assets/Scripts/Win/Win.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+   public Text Text;
+   public Canvas Canvas;
+
+
+   private void Awake()
+   {
+      Canvas.gameObject.SetActive(false);
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.CompareTag("Player"))
+      {
+         Canvas.gameObject.SetActive(true);
+          Text.text = "You WIN!";
+      }
+   }
+}
